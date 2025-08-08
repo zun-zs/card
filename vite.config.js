ed5f8246867126351c23c2b9b97160711c62aadb
zouzs <534920290@qq.com>
2025-08-08 14:32:14 +0800

    Add Texas Hold'em game components and build config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: './',
+  publicPath: '/',
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    outDir: 'holdem',//想要把dist修改成什么名字在这边改
   }
 })
